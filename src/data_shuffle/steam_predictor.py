123671fab020033b89434a3badffa7cf6621c165
Krystian_Wojakiewicz <krysh97@gmail.com>
2020-04-01 23:06:57 +0200

    Fix bug regarding the produce_encoded_dataframe method.

--- src/data_shuffle/steam_predictor.py
+++ src/data_shuffle/steam_predictor.py
@@ -14,19 +14,19 @@ def load_data(steam_path, file):
 def encode_attribute_from_column(dataframe_column):
     id_attribute_bi_dict = BiDict()
     encoded_column = []
-    for idx, attribute in enumerate(dataframe_column):
-        if attribute not in id_attribute_bi_dict.keys():
-            id_attribute_bi_dict[attribute] = idx
-            encoded_column.append(idx)
+    for idx, attr in enumerate(dataframe_column):
+        if attr not in id_attribute_bi_dict.keys():
+            id_attribute_bi_dict[attr] = idx
+        encoded_column.append(id_attribute_bi_dict[attr])
     return encoded_column, id_attribute_bi_dict
 
 
 # ## TODO
-# funckcja zapisująca słownik do pliku wraz z jej opisem
-def extract_values_from_columns(dataframe_column):
+# funkcja zapisująca słownik do pliku wraz z jej opisem
+def extract_values_from_columns(dataframe_column, value_sep=';'):
     unique_attribute_values = set()
     for attribute in dataframe_column:
-        for value in attribute.split(';'):
+        for value in attribute.split(value_sep):
             unique_attribute_values.add(value)
     return unique_attribute_values
 
@@ -34,8 +34,8 @@ def extract_values_from_columns(dataframe_column):
 def process_ratings_attribute(pos, neg):
     ratings = []
     for p, n in zip(pos, neg):
-        total_reviews = p + n
-        rating = np.round((p / total_reviews) * 100)
+        total_reviews = int(p) + int(n)
+        rating = np.round((int(p) / total_reviews) * 100)
         ratings.append(rating)
     return ratings
 
@@ -49,11 +49,11 @@ def process_release_date_attribute(release_date_column):
     return months, years
 
 
-def produce_encoded_dataframe(relevant_attributes, raw_data):
+def produce_encoded_dataframe(relevant_attributes, attributes_to_concat, raw_data, value_sep=';'):
     encoded_data = pd.DataFrame()
-    encoded_data = pd.concat([encoded_data, raw_data['platforms'].str.get_dummies(sep=';')], axis=1)
-    encoded_data = pd.concat([encoded_data, raw_data['categories'].str.get_dummies(sep=';')], axis=1)
-    encoded_data = pd.concat([encoded_data, raw_data['genres'].str.get_dummies(sep=';')], axis=1)
+    for attr in attributes_to_concat:
+        encoded_data = pd.concat([encoded_data, raw_data[attr].str.get_dummies(sep=value_sep)], axis=1)
+
     for attr in relevant_attributes.keys():
         encoded_data[attr] = relevant_attributes[attr]
     return encoded_data
@@ -61,27 +61,23 @@ def produce_encoded_dataframe(relevant_attributes, raw_data):
 
 def main():
     steam = ep.load_csv(ep.Paths.STEAM.value)
-
     steam_cp = steam.copy()
-    create_and_show_plot_from_dataframe_column(steam_cp['owners'], 'steam_cp_owners')
 
-    encoded_devs, _ = encode_attribute_from_column('developer')
-    encoded_publishers, _ = encode_attribute_from_column('publisher')
-    encoded_owners, _ = encode_attribute_from_column('owners')
+    encoded_devs, _ = encode_attribute_from_column(steam_cp['developer'])
+    encoded_publishers, _ = encode_attribute_from_column(steam_cp['publisher'])
+    encoded_owners, _ = encode_attribute_from_column(steam_cp['owners'])
     ratings = process_ratings_attribute(steam_cp['positive_ratings'], steam_cp['negative_ratings'])
     months, years = process_release_date_attribute(steam_cp['release_date'])
-
     steam_cp = steam_cp.drop(columns=['appid', 'name', 'steamspy_tags', 'price', 'release_date'])
+
     relevant_attributes = {'Month': months, 'Year': years,
                            'English': steam_cp['english'], 'Developer': encoded_devs,
-                           'Genres'
                            'Publisher': encoded_publishers, 'Required_Age': steam_cp['required_age'],
                            'Achievements': steam_cp['achievements'], 'Average_Playtime': steam_cp['average_playtime'],
                            'Median_Playtime': steam_cp['median_playtime'], 'Rating': ratings, 'Owners': encoded_owners}
-    produce_encoded_dataframe(relevant_attributes, steam_cp)
+    attributes_to_concat = ['genres', 'categories', 'platforms']
+    produce_encoded_dataframe(relevant_attributes, attributes_to_concat, steam_cp)
 
 
 if __name__ == '__main__':
     main()
-
-
